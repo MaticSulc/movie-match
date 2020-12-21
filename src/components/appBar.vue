13c533ef28434131271d6f3d3fba4456f21dae27
Matic <matic.sulc@sers.si>
2020-12-21 19:05:06 +0100

    Working app v1:sparkes:

---
+++ src/components/appBar.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <v-app-bar app color="secondary" dark>
+      <v-app-bar-nav-icon
+        @click="drawer = !drawer"
+        v-if="userId"
+      ></v-app-bar-nav-icon>
+      
+      <div class="d-flex align-center">
+        <router-link
+          style="text-decoration: none; color: #fff"
+          to="/home"
+          ><h2>Movie Match</h2></router-link
+        >
+      </div>
+
+      <v-spacer></v-spacer>
+      
+
+      <v-btn v-if="userId" @click="logoutUser" text>Logout</v-btn>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute bottom temporary>
+      <v-list nav dense>
+        <v-list-item-group active-class="primary">
+          <v-list-item to="/home"
+            ><v-list-item-title>Home</v-list-item-title></v-list-item
+          >
+          <v-list-item to="/matches"
+            ><v-list-item-title>Matches</v-list-item-title></v-list-item
+          >
+          <v-list-item to="/add-partner"
+            ><v-list-item-title>Add Partner</v-list-item-title></v-list-item
+          >
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+<script>
+import firebase from "firebase";
+export default {
+  data: () => ({
+    drawer: false,
+  }),
+  methods: {
+    async logoutUser() {
+      await firebase.auth().signOut();
+      this.$store.dispatch("user/clearUserData");
+      this.$router.replace("/");
+    },
+  },
+  computed: {
+    userId() {
+      return this.$store.state.user.id;
+    },
+  },
+};
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersist from 'vuex-persist'
+import user from './modules/user'
+import {vuexfireMutations} from "vuexfire"
 
 Vue.use(Vuex)
+
+const vuexLocal = new VuexPersist({
+  storage: window.localStorage
+})
 
 export default new Vuex.Store({
   state: {
   },
   mutations: {
+    ...vuexfireMutations,
   },
   actions: {
   },
   modules: {
-  }
+    user,
+  },
+  plugins: [vuexLocal.plugin],
 })
