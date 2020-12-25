4f18c1191d7bd39d61106ce170f651079ff44c44
Matic <matic.sulc@sers.si>
2020-12-25 14:03:54 +0100

    Changed firebase to production

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-import firebase from "firebase";
+import firebase from "firebase/app";
 import VueYouTubeEmbed from "vue-youtube-embed";
 
 Vue.config.productionTip = false;
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -58,7 +58,7 @@
 </template>
 
 <script>
-import firebase from "firebase";
+import firebase from "firebase/app";
 import { db } from "../main";
 export default {
   data: () => ({
